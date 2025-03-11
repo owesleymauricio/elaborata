f84df9246c2937d533d311ec0083a34f2a9052d9
owesleymauricio <wesleymauricio85@gmail.com>
2025-03-11 20:21:09 -0300

    inicio

---
+++ aula1.js
@@ -0,0 +1,19 @@
+let nome = "wesley";
+var idade = 65; //integer 
+let peso = 23.4; // float
+let andando = false;// boolean
+
+if (idade >= 65){
+    console.log('vovo')
+}else if( idade >= 18 && idade < 65){
+    console.log('adulto')
+    
+}else if (idade >= 12){
+    console.log('adolescente')
+
+}else{
+    console.log('criança')
+
+}
+
+
---
+++ aula2.js
@@ -0,0 +1,25 @@
+let alunos = ["pedro", "maria", "joao"]
+let alunoOrdenado = alunos.sort()
+let ini = 0
+let fim = alunoOrdenado.length 
+
+//while ( fim >= ini )
+//{
+//    console.log(alunos[ini])
+//
+//    ini = ini - 1
+//}
+
+
+//while ( ini < fim )
+//{
+//    console.log(alunos[ini])
+//
+//    ini = ini + 1
+//}
+
+
+// foreach so para array
+alunos.forEach(function(item){
+    console.log(item)
+})
