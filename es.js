d56181aff6c817f2f0cf84a1175d88c23d5f5fc3
owesleymauricio <wesleymauricio85@gmail.com>
2025-03-17 20:51:04 -0300

    aula

---
+++ es.js
@@ -0,0 +1,16 @@
+
+//array.forEach(element => {
+//    console.log(element)
+//});
+
+/// detructor
+//let [a, b] = exemplo // pega os itens por ordem
+
+
+//let novo = [...cidades, ...vetor]
+
+//let obj = {nome:"maria", idade: 23}
+
+   
+
+
--- exec96e98.js
+++ exec96e98.js
@@ -17,23 +17,47 @@
 //console.log("pares: ", par)
 //console.log("impares: ", impar)
 
-console.log("Digite 5 números inteiros separados por vírgulas: ");
+//console.log("Digite 5 números inteiros separados por vírgulas: ");
+//
+//process.stdin.on("data", function (data) {
+//    let input = data.toString("utf-8").trim(); 
+//
+//    if (input.toLowerCase() === "s") process.exit(); 
+//
+//    // splita depois mapeia transformado em number
+//    let numeros = input.split(",").map(num => parseInt(num.trim())); 
+//    // faz um reduce para somar 
+//    let soma = numeros.reduce((acc, num) => acc + num, 0); 
+//    // faz a multiĺicação
+//    let multiplicacao = numeros.reduce((acc, num) => acc * num, 1); 
+//
+//    console.log("\nNúmeros digitados:", numeros.join(" "));
+//    console.log(`A soma dos números é ${soma}`);
+//    console.log(`A multiplicação dos números é ${multiplicacao}`);
+//
+//    process.exit(); 
+//});
 
-process.stdin.on("data", function (data) {
-    let input = data.toString("utf-8").trim(); 
 
-    if (input.toLowerCase() === "s") process.exit(); 
+//Exercicio 097
+//Faça um Programa que peça as quatro notas de 10 alunos, calcule e
+//armazene num vetor a média de cada aluno, imprima o número de alunos
+//com média maior ou igual a 7.0.
 
-    // splita depois mapeia transformado em number
-    let numeros = input.split(",").map(num => parseInt(num.trim())); 
-    // faz um reduce para somar 
-    let soma = numeros.reduce((acc, num) => acc + num, 0); 
-    // faz a multiĺicação
-    let multiplicacao = numeros.reduce((acc, num) => acc * num, 1); 
+let alunos = [1,2,3,4,5,6,7,8,9,10]
+let notas = [1,2,3,4]
 
-    console.log("\nNúmeros digitados:", numeros.join(" "));
-    console.log(`A soma dos números é ${soma}`);
-    console.log(`A multiplicação dos números é ${multiplicacao}`);
+for(let aluno of alunos){
+    console.log(aluno)
+    let als = aluno
+    for(let nota of notas){
+    let nots = nota
 
-    process.exit(); 
-});
+    }
+
+    console.log(als)
+    console.log(nots)
+
+
+
+}
