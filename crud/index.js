2e846a7d4a592dc1d8810a87de8c7b6038fa49fc
owesleymauricio <wesleymauricio85@gmail.com>
2025-03-19 21:45:57 -0300

    crud

--- crud/index.js
+++ crud/index.js
@@ -1,16 +1,94 @@
 import sqlite3 from "sqlite3"
 import { open } from "sqlite"
 
+/**
+ * conecction
+ * @returns {database}
+ */
+const conn = async () => {
+   const db = await open({
+      filename: 'clientes.db',
+      driver: sqlite3.Database
+   })
+   return db
+}
 
-export function cadastrar() {
+/**
+ * cadastra usuario
+ * @param {string} nome 
+ * @param {string} cidade 
+ * @param {string} email 
+ * @param {number} idade 
+ * @param {string} telefone 
+ * @returns 
+ */
+export async function cadastrar(nome, cidade, email, idade, telefone) {
+   let query = `
+   INSERT 
+   INTO CLIENTE
+   (nome, cidade, email, idade, telefone)
+   VALUES
+   ($1, $2, $3, $4, $5)
+   `
+
+   try {
+      const db = await conn()
+
+      let req = await db.run(query, nome, cidade, email, idade, telefone);
+      return (req.changes === 1) ? true : false;
+
+   } catch (error) {
+      console.log("deu xabu: ",error);
+ 
+      return false
+   }
 
 }
 
-export function atualizar() {
+/**
+ * 
+ * @param {number} id 
+ * @param {json} colunas 
+ */
+export async function atualizar(id, colunas) {
 
+ 
+   let set = [];
+
+   if(colunas.telefone){
+     set.push("telefone = '"+ colunas.telefone + "'")
+   }
+   if(colunas.nome){
+     set.push("nome = '"+ colunas.nome + "'")
+   }
+   if(colunas.idade){
+     set.push("idade = '"+ colunas.idade + "'")
+   }
+   let query = `UPDATE cliente SET ${set.join(", ")} WHERE id = $1;`
+
+   const db = await conn();
+
+   const req = await db.run(query, id)
+
+   console.log(req)
+   return (req.changes == 1) ? true : false;
 }
 
-export function deletar() {
+/**
+ * deleta o usuario
+ * @param {number} id 
+ * @returns {boolean}
+ */
+export async function deletar(id) {
+   let query = `DELETE FROM cliente WHERE id = $1;`
+
+   const db = await conn(query);
+
+   const req = await db.run(query, id)
+
+   console.log(req)
+   return (req.changes == 1) ? true : false;
+   
 
 }
 
@@ -23,12 +101,9 @@ export function deletar() {
 export async function pesquisar(id) {
    let query = `SELECT * FROM cliente WHERE id = ${id}`
 
-   const db = await open({
-      filename: 'clientes.db',
-      driver: sqlite3.Database
-   })
+   const db = await conn();
 
-   return await db.get(query);
+   await db.get(query);
 
 }
 
@@ -40,12 +115,9 @@ export async function pesquisar(id) {
 export async function getAll() {
    let query = `SELECT * FROM cliente`;
 
-   const db = await open({
-      filename: 'clientes.db',
-      driver: sqlite3.Database
-   });
+   const db = await conn();
 
    return await db.all(query);
 
-   
+
 }
--- crud/teste.js
+++ crud/teste.js
@@ -1,20 +1,33 @@
-import {getAll, pesquisar} from "./index.js";
+import {atualizar, cadastrar, deletar, getAll, pesquisar} from "./index.js";
 
 // async function teste() {
     
 
-setTimeout(async ()=>{
-    let res = await  getAll();
+//setTimeout(async ()=>{
+//    let res = await  getAll();
+//
+//    console.log(res)
+//}, 5000)
+////}
+//
+////teste()
+//
+//setTimeout(async ()=>{
+//    let res = await  pesquisar(1)
+//
+//    console.log(res)
+//}, 2000)
+
+async function teste(){
+    let val = {
+        telefone: "(41) 1234-4321",
+        idade: 10
+    }
+
+    let res = await atualizar(6, val)
 
     console.log(res)
-}, 5000)
-//}
+}
 
-//teste()
-
-setTimeout(async ()=>{
-    let res = await  pesquisar(1)
-
-    console.log(res)
-}, 2000)
+teste()
 
