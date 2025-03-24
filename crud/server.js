5dc1696e7e7b76f7645983d4a78b38db26cbd9c6
owesleymauricio <wesleymauricio85@gmail.com>
2025-03-24 20:10:49 -0300

    sssss

--- crud/server.js
+++ crud/server.js
@@ -16,6 +16,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+let port = 3000;
+
+if(process.env.PROD == "true"){
+    port = 80
+}else{
+    port = 3000;
+}
+
+console.log(process.env)
 // route action
 app.get('/clientes', async (req, res) => {
 
